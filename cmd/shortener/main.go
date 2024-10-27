299a99032cef990a0506fd51dd837f1dc9cb8c04
Tony <phenloveu@gmail.com>
2024-10-27 18:57:23 +0300

    add GET + tests

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,69 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/PhenHF/url-shortener/internal/app"
 )
 
 
+func init() {
+	http.HandleFunc(`/`, app.ReturnShortUrl)
+	http.HandleFunc(`/{id}`, app.RedirectToOriginalUrl)
+}
+
+
 func main() {
-	http.HandleFunc(`/`, Handler)
-	http.HandleFunc(`/{id}`, Handler)
+	run()
+}
+
+func run() {
 	err := http.ListenAndServe(`:8080`, nil)
 	if err != nil {
 		panic(err)
 	}
 }
 
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-	const expectedHeader = "text/plain"
-	
-	// fmt.Println(app.OriginalShortURL)
-	// fmt.Println(app.ShortOriginalURL)
-
-	
-	if ct := r.Header.Get("Content-Type"); ct != expectedHeader {
-		fmt.Println("1")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	if r.Method == http.MethodGet {
-		fmt.Println(app.ShortOriginalURL)
-		if r.PathValue("id") == "" {
-			fmt.Println("2")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		originUrl, ok := app.ShortOriginalURL[r.PathValue("id")]
-		if !ok {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		w.Header().Set("Location", originUrl)
-		// w.WriteHeader(http.StatusTemporaryRedirect)		
-		// w.Write([]byte(originUrl))
-	
-	} else if r.Method == http.MethodPost {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-				
-		w.Header().Set("Content-Type", "text/plain")
-		
-		shortUrl := app.GetShortUrl()
-		
-		app.ShortOriginalURL[shortUrl] = string(body)
-		w.WriteHeader(http.StatusCreated)		
-		w.Write([]byte("http://localhost:8080/" + shortUrl))
-		return
-	}
-
-	w.WriteHeader(http.StatusBadRequest)
-}
---
+++ internal/app/handler.go
@@ -0,0 +1,67 @@
+package app
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+
+func checkContentType(w http.ResponseWriter, r *http.Request) {
+	const expectedHeader = "text/plain"
+	if ct := r.Header.Get("Content-Type"); ct != expectedHeader {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+
+func RedirectToOriginalUrl(w http.ResponseWriter, r *http.Request) {
+	checkContentType(w, r)
+
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if r.PathValue("id") == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	originUrl, ok := ShortOriginalURL[r.PathValue("id")]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Location", originUrl)
+
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func ReturnShortUrl(w http.ResponseWriter, r *http.Request) {
+	checkContentType(w, r)
+
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if len(body) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	
+	shortUrl := GetShortUrl()
+	
+	ShortOriginalURL[shortUrl] = string(body)
+	
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)		
+	w.Write([]byte("http://localhost:8080/" + shortUrl))
+}
