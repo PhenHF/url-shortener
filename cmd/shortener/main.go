ba6323c506707fc16a1cbd8ffa440949ca5b8955
Tony <phenloveu@gmail.com>
2024-11-11 19:12:19 +0300

    add zap logger

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,6 +7,7 @@ import (
 
 	config "github.com/PhenHF/url-shortener/internal/config"
 	handler "github.com/PhenHF/url-shortener/internal/handler"
+	logger "github.com/PhenHF/url-shortener/internal/logger"
 	middleware "github.com/PhenHF/url-shortener/internal/middleware"
 	service "github.com/PhenHF/url-shortener/internal/service"
 	storage "github.com/PhenHF/url-shortener/internal/storage"
@@ -14,18 +15,22 @@ import (
 
 func main() {
 	var urlStorage = storage.UrlStorage{}
-
 	rt := chi.NewRouter()
+	rt.Use(logger.RequestLogger)
 	rt.Use(middleware.CheckContentType)
 	rt.Post(`/`, handler.ReturnShortUrl(service.GetShortUrl, &urlStorage, config.NetAddress.ResultAddr))
 	rt.Get(`/{id}`, handler.RedirectToOriginalUrl(&urlStorage))
-
 	run(rt)
 }
 
-func run(rt *chi.Mux) {
+func run(rt *chi.Mux) error {
+	if err := logger.Initialize("INFO"); err != nil {
+		return err
+	}
+	
 	err := http.ListenAndServe(config.NetAddress.StartServer, rt)
 	if err != nil {
 		panic(err)
 	}
+	return nil
 }
---
+++ internal/logger/logger.go
@@ -0,0 +1,84 @@
+package logger
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+var Log *zap.Logger = zap.NewNop()
+
+type (
+	responseData struct {
+		statusCode int
+		size int
+	}
+
+	loggingResponseWriter struct {
+		http.ResponseWriter
+		responseData *responseData
+	}
+)
+
+func Initialize(level string) error {
+	lvl, err := zap.ParseAtomicLevel(level)
+	if err != nil {
+		return err
+	}
+
+	cfg := zap.NewProductionConfig()
+
+	cfg.Level = lvl
+
+	zl, err := cfg.Build()
+	if err != nil {
+		return err
+	}
+
+	Log = zl
+	return nil
+}
+
+func (lrw *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := lrw.ResponseWriter.Write(b)
+	lrw.responseData.size += size
+	return size, err
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(statusCode int) {
+	lrw.ResponseWriter.WriteHeader(statusCode)
+	lrw.responseData.statusCode = statusCode
+}
+
+func RequestLogger(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		method := r.Method
+		uri := r.RequestURI
+
+		resData := &responseData{
+			size: 0,
+			statusCode: 0,
+		}
+
+		lwr := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData: resData,
+		}
+
+		h.ServeHTTP(&lwr, r)
+		
+		duration := time.Since(start)
+		Log.Info("request",
+			zap.String("method", method),
+			zap.String("uri", uri),
+			zap.String("duration", duration.String()),
+		)
+		Log.Info("response", 
+			zap.String("statusCode", strconv.Itoa(resData.statusCode)),
+			zap.String("response size", strconv.Itoa(resData.size)),
+		)
+	})
+} 
